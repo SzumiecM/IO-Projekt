c430398214496eca8e1aacdd4b5dc864d2a2b43b
SzumiecM <milosz.sz.m@gmail.com>
2020-11-23 00:07:20 +0100

    got rid of useless stuff

--- opencv_object_tracking.py
+++ opencv_object_tracking.py
@@ -11,54 +11,57 @@ import time
 import cv2
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str,
-	help="path to input video file", default='grupaB1.mpg')
-ap.add_argument("-t", "--tracker", type=str, default="kcf",
-	help="OpenCV object tracker type")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-v", "--video", type=str,
+# 	help="path to input video file", default='grupaB1.mpg')
+# ap.add_argument("-t", "--tracker", type=str, default="kcf",
+# 	help="OpenCV object tracker type")
+# args = vars(ap.parse_args())
+#
+# # extract the OpenCV version info
+# (major, minor) = cv2.__version__.split(".")[:2]
+#
+# # if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
+# # function to create our object tracker
+# if int(major) == 3 and int(minor) < 3:
+# 	tracker = cv2.Tracker_create(args["tracker"].upper())
+#
+# # otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
+# # approrpiate object tracker constructor:
+# else:
+# 	# initialize a dictionary that maps strings to their corresponding
+# 	# OpenCV object tracker implementations
+# 	OPENCV_OBJECT_TRACKERS = {
+# 		"csrt": cv2.TrackerCSRT_create,
+# 		"kcf": cv2.TrackerKCF_create,
+# 		"boosting": cv2.TrackerBoosting_create,
+# 		"mil": cv2.TrackerMIL_create,
+# 		"tld": cv2.TrackerTLD_create,
+# 		"medianflow": cv2.TrackerMedianFlow_create,
+# 		"mosse": cv2.TrackerMOSSE_create
+# 	}
+#
+# 	# grab the appropriate object tracker using our dictionary of
+# 	# OpenCV object tracker objects
+# 	tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
 
-# extract the OpenCV version info
-(major, minor) = cv2.__version__.split(".")[:2]
-
-# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
-# function to create our object tracker
-if int(major) == 3 and int(minor) < 3:
-	tracker = cv2.Tracker_create(args["tracker"].upper())
-
-# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
-# approrpiate object tracker constructor:
-else:
-	# initialize a dictionary that maps strings to their corresponding
-	# OpenCV object tracker implementations
-	OPENCV_OBJECT_TRACKERS = {
-		"csrt": cv2.TrackerCSRT_create,
-		"kcf": cv2.TrackerKCF_create,
-		"boosting": cv2.TrackerBoosting_create,
-		"mil": cv2.TrackerMIL_create,
-		"tld": cv2.TrackerTLD_create,
-		"medianflow": cv2.TrackerMedianFlow_create,
-		"mosse": cv2.TrackerMOSSE_create
-	}
-
-	# grab the appropriate object tracker using our dictionary of
-	# OpenCV object tracker objects
-	tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+tracker = cv2.TrackerKCF_create()
 
 # initialize the bounding box coordinates of the object we are going
 # to track
 initBB = None
 
-# if a video path was not supplied, grab the reference to the web cam
-if not args.get("video", False):
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=0).start()
-	time.sleep(1.0)
+# # if a video path was not supplied, grab the reference to the web cam
+# if not args.get("video", False):
+# 	print("[INFO] starting video stream...")
+# 	vs = VideoStream(src=0).start()
+# 	time.sleep(1.0)
+#
+# # # otherwise, grab a reference to the video file
+# else:
+# 	vs = cv2.VideoCapture(args["video"])
 
-# # otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-#vs = cv2.VideoCapture('grupaB1.mpg')
+vs = cv2.VideoCapture('grupaB1.mpg')
 
 # initialize the FPS throughput estimator
 fps = None
@@ -67,9 +70,9 @@ fps = None
 while True:
 	# grab the current frame, then handle if we are using a
 	# VideoStream or VideoCapture object
-	frame = vs.read()
+	frame = vs.read()[1]
 	#cv2.imshow("Frame", frame)
-	frame = frame[1] if args.get("video", False) else frame
+	# frame = frame[1] if args.get("video", False) else frame
 
 	# check to see if we have reached the end of the stream
 	if frame is None:
@@ -99,17 +102,17 @@ while True:
 
 		# initialize the set of information we'll be displaying on
 		# the frame
-		info = [
-			("Tracker", args["tracker"]),
-			("Success", "Yes" if success else "No"),
-			("FPS", "{:.2f}".format(fps.fps())),
-		]
+		# info = [
+		# 	("Tracker", args["tracker"]),
+		# 	("Success", "Yes" if success else "No"),
+		# 	("FPS", "{:.2f}".format(fps.fps())),
+		# ]
 
 		# loop over the info tuples and draw them on our frame
-		for (i, (k, v)) in enumerate(info):
-			text = "{}: {}".format(k, v)
-			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+		# for (i, (k, v)) in enumerate(info):
+		# 	text = "{}: {}".format(k, v)
+		# 	cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+		# 		cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
