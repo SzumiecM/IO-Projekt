f88e020d623ed1a3c2ae8ad603b5d3dc20a53b9a
SzumiecM <milosz.sz.m@gmail.com>
2020-12-03 23:31:39 +0100

    progress counter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 from time import time
+from scipy.spatial import distance as dist
+from collections import OrderedDict
 from imutils.object_detection import non_max_suppression
 
 
@@ -11,6 +13,111 @@ from imutils.object_detection import non_max_suppression
 #           wydaje się być koniecznością, program totalnie wywala się przy example01_mp4 (nie jest konieczny ale
 #           fajnie żeby działał jednak zawsze)
 #       - dodać licznik osób na obrazie
+class CentroidTracker:
+    def __init__(self, maxFramesDisappeared=60): #TODO do zwiększenia??
+        # counter to assign unique IDs to each person
+        self.nextObjectID = 0
+        # dictionary that stores person ID as the key and the centroid (x,y) coordinates as val
+        self.objects = OrderedDict()
+        # stores the information for how long a particular person has ben marked as lost
+        self.disappeared = OrderedDict()
+        # the number of consecutive frames a person is allowed to be marked as lost/disappeared
+        # until we deregister it
+        self.maxFramesDisappeared = maxFramesDisappeared
+
+    def register(self, centroid):
+        # use the next available UID
+        self.objects[self.nextObjectID] = centroid
+        self.disappeared[self.nextObjectID] = 0
+        self.nextObjectID += 1
+
+    def deregister(self, objectID):
+        # to deregister person we delete the object ID from both dict
+        del self.objects[objectID]
+        del self.disappeared[objectID]
+
+    def update(self, boxes):
+        # rects - list of centerX, centerY coordinates of bounding boxes
+        # first - check if it is empty
+        if len(boxes) == 0:
+            # loop over existing tracked people and mark then as disappeared
+            for objectID in list(self.disappeared.keys()):
+                self.disappeared[objectID] += 1
+
+                # deregister the person if it reached maximum number of frames where it
+                # has been marked as missing
+                if self.disappeared[objectID] > self.maxFramesDisappeared:
+                    self.deregister(objectID)
+
+            return self.objects
+
+        # create an array of input centroids for the current frame
+
+        inputCentroids = boxes
+        # if we are currently not tracking any people take the input centroids
+        # and register them
+        if len(self.objects) == 0:
+            for i in range(0, len(boxes)):
+                self.register(inputCentroids[i])
+
+        # if not, then we are currently tracking objects so we need to
+        # try and match the input centroids to existing people centroids
+        else:
+            # take the set of people IDs and corresponding centroids
+            objectIDs = list(self.objects.keys())
+            objectCentroids = list(self.objects.values())
+
+            # compute the distance between each pair of people centroids
+            # and input centroids, our goal is to match an input centroid to an
+            # existing person centroid
+            D = dist.cdist(np.array(objectCentroids), inputCentroids)
+
+            rows = D.min(axis=1).argsort()
+
+            cols = D.argmin(axis=1)[rows]
+
+            usedRows = set()
+            usedCols = set()
+
+            for (row, col) in zip(rows, cols):
+                # if we have already examined either the row or
+                # column value before, ignore it
+                # val
+                if row in usedRows or col in usedCols:
+                    continue
+                # otherwise, grab the object ID for the current row,
+                # set its new centroid, and reset the disappeared
+                # counter
+                objectID = objectIDs[row]
+                self.objects[objectID] = inputCentroids[col]
+                self.disappeared[objectID] = 0
+                # indicate that we have examined each of the row and
+                # column indexes, respectively
+                usedRows.add(row)
+                usedCols.add(col)
+
+            unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+            unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+
+            if D.shape[0] >= D.shape[1]:
+                # loop over the unused row indexes
+                for row in unusedRows:
+                    # grab the object ID for the corresponding row
+                    # index and increment the disappeared counter
+                    objectID = objectIDs[row]
+                    self.disappeared[objectID] += 1
+                    # check to see if the number of consecutive
+                    # frames the object has been marked "disappeared"
+                    # for warrants deregistering the object
+                    if self.disappeared[objectID] > self.maxFramesDisappeared:
+                        self.deregister(objectID)
+
+            else:
+                for col in unusedCols:
+                    self.register(inputCentroids[col])
+            # return the set of trackable objects
+        return self.objects
+
 
 class Person:
 
@@ -42,8 +149,15 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 cv2.startWindowThread()
 
-cap = cv2.VideoCapture('example_01.mp4')
+cap = cv2.VideoCapture('grupaB1.mpg')
 video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+
+ct = CentroidTracker()
 
 sizes = []
 avg_size = 0
@@ -52,13 +166,13 @@ times = []
 counted = []
 people = []
 
-net = cv2.dnn.readNetFromDarknet('yolov2-tiny.cfg', 'yolov2-tiny.weights')
+net = cv2.dnn.readNetFromDarknet('yolov4-tiny.cfg', 'yolov4-tiny.weights')
 LABELS = open('coco.names').read().strip().split("\n")
 
 our_confidence = 0.6
 our_threshold = 0.1
 
-for i in range(video_length - 1):
+for frame_number in range(1, video_length):
     counter = 0
     start = time()
     ret, frame = cap.read()
@@ -76,7 +190,7 @@ for i in range(video_length - 1):
 
     boxes = []
     confidences = []
-    classIDs = []
+    centroids = []
 
     # # resizing for faster detection
     # frame = cv2.resize(frame, (640, 480))
@@ -108,9 +222,9 @@ for i in range(video_length - 1):
                 y = int(centerY - (height / 2))
                 # update our list of bounding box coordinates, confidences,
                 # and class IDs
+                # centroids.append((centerX,centerY))
                 boxes.append([x, y, int(width), int(height)])
                 confidences.append(float(confidence))
-                classIDs.append(classID)
 
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, our_confidence,
                             our_threshold)
@@ -121,15 +235,25 @@ for i in range(video_length - 1):
             # extract the bounding box coordinates
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
+            centroids.append((boxes[i][0] + boxes[i][2] / 2, boxes[i][1] + boxes[i][3] / 2))
             # draw a bounding box rectangle and label on the image
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
             counter += 1
+
+    objects = ct.update(centroids)
+
+    for (objectID, centroid) in objects.items():
+        text = "ID {}".format(objectID)
+        cv2.putText(frame, text, (int(centroid[0]) - 10, int(centroid[1]) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.circle(frame, (int(centroid[0]), int(centroid[1])), 4, (0, 255, 0), -1)
 
     cv2.putText(frame, f'1 person' if counter == 1 else f'{counter} people', (10, 10),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.5, (0, 0, 255), 2)
 
     cv2.imshow('frame', frame)
+    out.write(frame)
 
     end = time()
     times.append(end - start)
@@ -137,6 +261,8 @@ for i in range(video_length - 1):
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    print(f'Progress: {"{:.2f}".format(100*frame_number/video_length)}%')
 
 cap.release()
 cv2.destroyAllWindows()
